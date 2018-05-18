103b22bc1b688017574d3026870f0ed598d84e2a
Victoria Daug <victoria.daug@gmail.com>
2018-05-18 22:01:33 +0100

    queues with stacks

--- Queue.js
+++ Queue.js
@@ -54,6 +54,39 @@ function display(q) {
   return result;
 }
 
+function isStackEmpty(stack) {
+    return stack.top === null;
+}
+
+class QueueStack {
+    constructor() {
+        this.inbox = new Stack();
+        this.outbox = new Stack();
+    }
+
+    enque(data) {
+        this.inbox.push(data)
+    }
+
+    _reverse() {
+        if(isStackEmpty(this.outbox)) {
+            while(!isStackEmpty(this.inbox)){
+                this.outbox.push(this.inbox.pop())
+            }
+        }
+    }
+
+	deque() {
+		this._reverse();
+		return this.outbox.pop();
+    }
+
+	peek() {
+		this._reverse();
+		return this.outbox.peek();
+    }
+}
+
 function main(){
   const starTrek = new Queue()
   starTrek.enqueue('Kirk')
